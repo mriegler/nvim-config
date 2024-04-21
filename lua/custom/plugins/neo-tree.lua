8049a0af3a8b98616b5fdc8b63398f5d60370d7e
Marcel Riegler <marcel.riegler@port-neo.com>
2024-04-21 22:10:16 +0200

    refact

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
+  branch = "*",
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
@@ -8,32 +8,26 @@ return {
     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
   },
   config = function()
-    vim.fn.sign_define("DiagnosticSignError",
-      { text = " ", texthl = "DiagnosticSignError" })
-    vim.fn.sign_define("DiagnosticSignWarn",
-      { text = " ", texthl = "DiagnosticSignWarn" })
-    vim.fn.sign_define("DiagnosticSignInfo",
-      { text = " ", texthl = "DiagnosticSignInfo" })
-    vim.fn.sign_define("DiagnosticSignHint",
-      { text = "󰌵", texthl = "DiagnosticSignHint" })
+    vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
+    vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
+    vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
+    vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
 
-    vim.keymap.set('n', '<leader>o', function()
-        if vim.bo.filetype == "neo-tree" then
-          vim.cmd.wincmd "p"
-        else
-          vim.cmd.Neotree "focus"
-        end
-      end,
-      { desc = '[O]pen File Explorer' })
+    vim.keymap.set("n", "<leader>o", function()
+      if vim.bo.filetype == "neo-tree" then
+        vim.cmd.wincmd "p"
+      else
+        vim.cmd.Neotree "focus"
+      end
+    end, { desc = "[O]pen File Explorer" })
 
-
-    require("neo-tree").setup({
+    require("neo-tree").setup {
       window = {
-        position = "right",
+        position = "float",
         mappings = {
           ["o"] = "open",
         },
-      }
-    })
-  end
+      },
+    }
+  end,
 }
